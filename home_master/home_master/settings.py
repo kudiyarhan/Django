0620053eb671c983aaea6eb82e3972b0855ac978
kudiyarhan <romeo333.91@gmail.com>
2026-07-13 19:50:00 +0000

    fix: production settings (CSRF, favicon, templates) and dependencies

--- home_master/home_master/settings.py
+++ home_master/home_master/settings.py
@@ -94,10 +94,13 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if not DEBUG:
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
