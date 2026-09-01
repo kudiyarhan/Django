f04966a26207a1adadbde17e3bed4674aef327db
kudiyarhan <romeo333_91@mail.ru>
2026-09-01 22:19:35 +0300

    Adding TV, coffee, laptop, 3D functions.

--- home_master/blog/urls.py
+++ home_master/blog/urls.py
@@ -6,6 +6,11 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.all_articles, name='all_articles'),
+    path('tv/', views.tv_repair, name='tv'),
+    path('cofe/', views.cofe_repair, name='cofe'),
+    path('3dprinter/', views.printer_repair, name='3dprinter'),
+    path('laptop/', views.laptop_repair, name='laptop'),
     path('<slug:slug>/', views.article, name='article'),
+
 
 ]
--- home_master/blog/views.py
+++ home_master/blog/views.py
@@ -1,10 +1,4 @@
-# from django.shortcuts import render
-#
-# def all_articles(request):
-#     return render(request, 'blog/all_articles.html')
-#
-# def article(request):
-#     return render(request, 'blog/article.html')
+
 
 from django.shortcuts import render, get_object_or_404
 from .models import Article
@@ -18,3 +12,26 @@ def all_articles(request):
 def article(request, slug):
     article = get_object_or_404(Article, slug=slug)  # Ищем статью по slug
     return render(request, 'blog/article.html', {'article': article})
+
+# def tv_repair(request):
+#     return render(request, 'blog/tv.html')
+def tv_repair(request):
+    # Получаем только опубликованные статьи, у которых категория имеет slug 'tv'
+    articles = Article.objects.filter(category__slug='tv')
+
+    return render(request, 'blog/tv.html', {'articles': articles})
+
+
+def cofe_repair(request):
+    articles = Article.objects.filter(category__slug='cofe')
+    return render(request, 'blog/cofe.html', {'articles': articles})
+
+
+def printer_repair(request):
+    articles = Article.objects.filter(category__slug='3dprinter')
+    return render(request, 'blog/3dprinter.html', {'articles': articles})
+
+
+def laptop_repair(request):
+    articles = Article.objects.filter(category__slug='laptop')
+    return render(request, 'blog/laptop.html', {'articles': articles})
